c0d1a6b95d6cdfb4dc51e4920178fe39f06b9da4
Colin Densem <colin@summit360.co.uk>
2019-10-19 12:43:22 +0100

    feat: add navbar component

---
+++ components/TheNavbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="nav">
+    <div class="mb1 bg-green h1"></div>
+    <div class="logo"></div>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
+  <div class="page">
+    <TheNavbar />
     <nuxt />
   </div>
 </template>
+
+<script>
+import TheNavbar from '~/components/TheNavbar'
+export default {
+  components: {
+    TheNavbar
+  }
+}
+</script>
 
 <style>
 html {
